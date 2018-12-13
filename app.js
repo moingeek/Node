05eb6524a2245b5de360f1fc9f2f6aaa71c256e7
moingeek <gadmoin@gmail.com>
2018-12-13 16:25:11 +0530

    Setting up Server using Express and playing with middlewares

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-const http = require('http');
 
 const express = require('express');
 
@@ -11,8 +10,8 @@ app.use((req,res,next) => {
 
 app.use((req,res,next) => {
     console.log("Hello from middleware");
+    res.send('<h1> I am Expressing</h1>');
 });
 
-const server = http.createServer(app);
 
-server.listen(3000);
+app.listen(3000);
