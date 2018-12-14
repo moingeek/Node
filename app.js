c547e521b028a147b4325016cd64aa60ba42b3e8
moingeek <gadmoin@gmail.com>
2018-12-14 14:33:58 +0530

    Working with middlewares done

--- app.js
+++ app.js
@@ -3,12 +3,16 @@ const express = require('express');
 
 const app = express();
 
-app.use((req,res,next) => {
-    console.log("Hello from express");
+app.use('/',(req,res,next) => {
+    console.log("This always runs");
     next();
 });
 
-app.use((req,res,next) => {
+app.use('/add-product',(req,res,next) => {
+    res.send('<h1> I am at add-product page</h1>');
+});
+
+app.use('/',(req,res,next) => {
     console.log("Hello from middleware");
     res.send('<h1> I am Expressing</h1>');
 });
