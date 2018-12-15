1bb910a8246c275c80c8cf1285ba617463605099
moingeek <gadmoin@gmail.com>
2018-12-15 18:00:37 +0530

    Setting up ejs and includes

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
 
+app.set('view engine','ejs');
+app.set('views','views');
+
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop')
 
@@ -14,7 +17,7 @@ app.use('/admin',adminData.routes);
 app.use(shopRoutes);
 
 app.use((req,res,next) => {
-    res.status(404).sendFile(path.join(__dirname,'views','404.html'));
+    res.status(404).render('404',{pageTitle: '404'});
 });
 
 
