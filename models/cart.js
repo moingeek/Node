ce3007100cf1bea9ad0e47a749c9c5df54431561
moingeek <gadmoin@gmail.com>
2018-12-19 14:47:20 +0530

    Cart page

--- models/cart.js
+++ models/cart.js
@@ -40,14 +40,28 @@ module.exports = class Cart {
             }
             const updatedCart = {...JSON.parse(fileContent)};
             const product = updatedCart.products.findIndex(prod => prod.id === id);
+            if (!product) {
+                return;
+            }
             const productQty = product.qty;
             updatedCart.products = updatedCart.products.filter(
-                prob => prod.id !== id
+                prod => prod.id !== id
             );
             updatedCart.totalPrice = updatedCart.totalPrice - productPrice * productQty;
-            fs.writeFile(p, JSON.stringify(updatedcart), err => {
+            fs.writeFile(p, JSON.stringify(updatedCart), err => {
                 console.log(err);
             }); 
         }
     );}
+
+    static getCart(cb) {
+        fs.readFile(p, (err, fileContent) => {
+            const cart = JSON.parse(fileContent);
+            if(err) {
+                cb(null);
+            } else {
+                cb(cart);
+            }
+        });
+    }
 }
