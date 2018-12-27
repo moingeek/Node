54ce68cbbea965657e9c21da67fa2e0e617fc5e1
moingeek <gadmoin@gmail.com>
2018-12-27 17:19:07 +0530

    Adding user model and creating dummy user.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const errorsController = require('./controllers/error');
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,13 +18,31 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req,res,next) => {
+    User.findById(1).then(user => {
+        req.user = user;
+        next();
+    }).catch(err => console.log(err));
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorsController.get404);
 
+Product.belongsTo(User,{ constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
+
+// This is just migrations what we call in laravel.
 sequelize.sync().then(result => {
+    return User.findById(1);
     // console.log(result);
+}).then(user => {
+    if(!user) {
+       return User.create({name: 'Moin', email:'test@test.com'});
+    }
+    return user;
+}).then(user => {
     app.listen(3000);
 }).catch(err => {
     console.log(err);
--- controllers/admin.js
+++ controllers/admin.js
@@ -13,7 +13,7 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  Product.create({
+  req.user.createProduct({
     title: title,
     imageUrl: imageUrl,
     price: price,
@@ -29,8 +29,11 @@ exports.getEditProduct = (req, res, next) => {
     return res.redirect('/');
   }
   const prodId = req.params.productId;
-  Product.findById(prodId)
-    .then(product => {
+  req.user.
+    getProducts({where: {id: prodId}})
+  //Product.findById(prodId)
+    .then(products => {
+      const product = products[0];
       if(!product) {
         return res.redirect('/');
       }
@@ -62,7 +65,7 @@ exports.postEditProduct = (req,res,next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll().then(products => {
+  req.user.getProducts().then(products => {
     res.render('admin/products', {
       prods: products,
       pageTitle: 'Admin Products',
