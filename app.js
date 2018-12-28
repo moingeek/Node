a61bcecfe6b8c1afdf1d0fcc85d39576ad42f199
moingeek <gadmoin@gmail.com>
2018-12-28 14:40:15 +0530

    Making cart model and cart item model and making associations between them.

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ const errorsController = require('./controllers/error');
 const sequelize = require('./util/database');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
+
 
 const app = express();
 
@@ -32,6 +35,10 @@ app.use(errorsController.get404);
 
 Product.belongsTo(User,{ constraints: true, onDelete: 'CASCADE'});
 User.hasMany(Product);
+User.hasOne(Cart);
+Cart.belongsTo(User);
+Cart.belongsToMany(Product, {through: CartItem});
+Product.belongsToMany(Cart , {through: CartItem});
 
 // This is just migrations what we call in laravel.
 sequelize.sync().then(result => {
