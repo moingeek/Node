7d9c072ec5fc55a515634ef56e827ff0115331fc
moingeek <gadmoin@gmail.com>
2019-01-21 11:22:17 +0530

    After reinstalltion

--- util/database.js
+++ util/database.js
@@ -1,5 +1,5 @@
-const Sequelize = require('sequelize');
+// const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize('node-complete','root','root',{dialect: 'mysql', host: 'localhost'});
+// const sequelize = new Sequelize('node-complete','root','root',{dialect: 'mysql', host: 'localhost'});
 
-module.exports = sequelize;
+// module.exports = sequelize;
