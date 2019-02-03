2e6336da61a5b6c52e92d91c6208571cf0442ab7
moingeek <gadmoin@gmail.com>
2019-02-03 17:21:49 +0530

    Configuring Cookes, Adding express-session package

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,13 @@
 exports.getLogin = (req, res, next) => {
-  const isLoggedin = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
+  // const isLoggedin = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true';
   res.render('auth/login', {
     path: '/login',
     pageTitle: 'Login',
-    isAuthenticated: isLoggedin
+    isAuthenticated: false
   });
 };
 
 exports.postLogin = (req,res,next) => {
-  res.setHeader('Set-cookie','loggedin=true');
+  res.setHeader('Set-cookie','loggedin=true; HttpOnly');
   res.redirect('/');
 }
