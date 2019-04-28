f17f61b2389f024dc189d47e66f49841762f6d1c
moingeek <gadmoin@gmail.com>
2019-04-28 12:28:18 +0530

    Flitering Files using MIME Types

--- controllers/admin.js
+++ controllers/admin.js
@@ -17,11 +17,25 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
-  const imageUrl = req.body.image;
+  const image = req.file;
   const price = req.body.price;
   const description = req.body.description;
+  if (!image) {
+    return res.status(422).render('admin/edit-product', {
+      pageTitle: 'Add Product',
+      path: '/admin/add-product',
+      editing: false,
+      hasError: true,
+      product: {
+        title: title,
+        price: price,
+        description: description
+      },
+      errorMessage: 'Attached file is not an image',
+      validationErrors: errors.array()
+    });
+  }
   const errors = validationResult(req);
-
   if (!errors.isEmpty()) {
     console.log(errors.array());
     return res.status(422).render('admin/edit-product', {
